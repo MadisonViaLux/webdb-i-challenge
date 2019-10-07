91a018f929b36dbc21bc93bcc636573c41e58c38
MadisonViaLux <madison.s.mckown@gmail.com>
2019-10-07 13:13:44 -0700

    10/72019 reset project

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
+  
 const express = require('express');
 
-const db = require('./work/work-router');
+const db = require('./data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
-
-server.use('/', db);
 
 module.exports = server;
